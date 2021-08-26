02e83979654f286bc7cfe010eb202854eba39162
nolus <n_reijnders@hotmail.com>
2021-08-26 20:16:31 +0200

    test(turnRight): add unit tests for turning right

---
+++ test/rover.spec.js
@@ -0,0 +1,58 @@
+class Rover {
+    dirIndex;
+    xCoord;
+    yCoord;
+
+    constructor(xCoord, yCoord, dirIndex){
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.dirIndex = dirIndex;
+    }
+
+    turnRight(){
+        this.dirIndex++;
+    }
+}
+
+describe('mars rover', () => {
+    it('should have dirIndex of 1 when it was 0 and turning right', () => {
+        const rover = new Rover(1, 2, 0);
+        rover.turnRight();
+        expect(rover.dirIndex).toBe(1);
+    })
+
+    it('should have dirIndex of 2 when it was 1 and turning right', () => {
+        const rover = new Rover(1, 2, 1);
+        rover.turnRight();
+        expect(rover.dirIndex).toBe(2);
+    })
+
+    it('should have dirIndex of 3 when it was 2 and turning right', () => {
+        const rover = new Rover(1, 2, 2);
+        rover.turnRight();
+        expect(rover.dirIndex).toBe(3);
+    })
+
+    it('should have dirIndex of 4 when it was 3 and turning right', () => {
+        const rover = new Rover(1, 2, 3);
+        rover.turnRight();
+        expect(rover.dirIndex).toBe(4);
+    })
+})
+
+
+/*
+Test cases
+
+Direction = [North, East, South, West]
+
+1. turn right > directionIndex + 1
+    a. N > E
+    b. E > S
+    c. S > W
+    d. W > N
+2. turn left > directionIndex - 1
+3. move forward > switch case > index=0 then yCoord + 1, etc....
+4. move backward > switch case > index=0 then yCoord - 1, etc....
+
+*/
